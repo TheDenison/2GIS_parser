8ebbd4f8ee751b0cfa87e9dd205263ebe4560829
Денис <sergeev.denison@mail.ru>
2023-08-18 17:04:16 +0300

    beta ver

--- card_finder.py
+++ card_finder.py
@@ -1,7 +1,11 @@
+import datetime
 import json
 import os
+import random
 import re
-
+from urllib.parse import urlparse
+import pandas as pd
+from soup_content import SoupContent
 import math
 import requests
 from bs4 import BeautifulSoup
@@ -10,43 +14,144 @@ from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 from urllib3.util import url
+from openpyxl import Workbook
+from openpyxl.styles import Font, PatternFill
 
 from constants import districts, type_org_mapping
 
 
-# # Асинхронная функция для запроса через aiohttp
-# async def fetch(session, url):
-#     async with session.get(url) as response:
-#         return await response.text()
-#
-#
-# # Синхронная функция для запроса через Selenium
-# def fetch_with_selenium(url):
-#     driver = webdriver.Chrome()
-#     driver.get(url)
-#     content = driver.page_source
-#     driver.close()
-#     return content
-#
-#
-# # Асинхронная функция для запуска синхронной функции в потоке
-# async def fetch_with_selenium_async(url):
-#     loop = asyncio.get_event_loop()
-#     return await loop.run_in_executor(None, fetch_with_selenium, url)
-#
-#
-# async def main():
-#     async with aiohttp.ClientSession() as session:
-#         html_content = await fetch(session, 'https://2gis.ru/moscow')
-#         soup = BeautifulSoup(html_content, 'lxml')
-#         # Добавьте код парсинга с использованием soup здесь
-#
-#         selenium_content = await fetch_with_selenium_async('https://2gis.ru/moscow')
-#         # Обработайте содержимое selenium_content
-
 def filter_links(hrefs):
     pattern = re.compile(r'^https://2gis\.ru/.+/firm/.+')
     return [href for href in hrefs if pattern.match(href)]
+
+
+# def check_new_firm():
+#     with open('firms_dict.json') as file:
+#         firms_list = json.load(file)
+#     print(firms_list)
+
+
+class Parser:
+    def __init__(self, driver):
+        self.driver = driver
+        self.soup_data = SoupContent()
+
+    def host(self, urls):
+        self.driver.maximize_window()
+        self.driver.get('https://2gis.ru/moscow')
+        parent_handle = self.driver.window_handles[0]
+        org_id = 0
+        outputs = []
+        for firm_url in urls:
+            try:
+
+                self.driver.execute_script(f'window.open("{firm_url}","org_tab");')
+                child_handle = [x for x in self.driver.window_handles if x != parent_handle][0]
+                self.driver.switch_to.window(child_handle)
+                sleep(random.uniform(0.2, 0.8))
+                soup = BeautifulSoup(self.driver.page_source, "lxml")
+
+                org_id += 1
+                name = self.soup_data.get_name(soup)
+                print(f"название: {name}")
+                address = self.soup_data.get_address(soup)
+                print(f'адрес: {address}')
+                link = website = self.soup_data.get_website(soup)
+                print(f'сайт: {website}')
+                twogis = self.driver.current_url
+                print(f'сслыка: {twogis}')
+                rating = self.soup_data.get_rating(soup)
+                print(f'Оценка: {rating}')
+                email = self.soup_data.get_email(soup)
+                print(f'Емаил: {email}')
+                branch = self.soup_data.get_branch(soup)
+                print(f'Филиалы: {branch}')
+                ip_inn = ''
+                reviews = self.soup_data.get_reviews(soup)
+                print(f'Отзывы: {reviews}')
+                category = self.soup_data.get_category(soup)
+                print(f'Категория: {category}')
+                phones = self.get_phones()
+                # phone = self.soup_data.get_phone(soup)
+                print(f'Номер: {phones}')
+
+                station = self.soup_data.get_station(soup)
+                print(f'Станция: {station}')
+
+                if (len(link)) != 0:
+                    parsed_url = urlparse(link)
+                    if not parsed_url.scheme:
+                        link = 'https://' + link
+                    ip_inn = self.soup_data.get_ip_inn(link)
+                    if ip_inn == None:
+                        ip_inn = ''
+                    print(f'ИН/ИНН: {ip_inn}\n')
+
+                output = [org_id, category, name, address, website, ip_inn, branch, twogis,
+                          rating, reviews, phones, email]
+                outputs.append(output)
+                if len(outputs) % 50 == 0:
+                    self.driver.quit()
+                    sleep(random.uniform(2.2, 2.4))
+                    self.driver = webdriver.Chrome()
+                    self.driver.maximize_window()
+                    self.driver.get('https://2gis.ru/moscow')
+                    parent_handle = self.driver.window_handles[0]
+
+                self.driver.switch_to.window(parent_handle)
+                sleep(random.uniform(0.2, 0.4))
+
+            except:
+                print('except')
+                self.driver.quit()
+                sleep(random.uniform(2.2, 2.4))
+                self.driver = webdriver.Chrome()
+                self.driver.maximize_window()
+                self.driver.get('https://2gis.ru/moscow')
+                parent_handle = self.driver.window_handles[0]
+        self.table_exel(outputs)
+        print('Данные сохранены')
+        # self.driver.quit()
+
+    def get_phones(self):
+        phones = ""
+        try:
+            # while not phones:
+            self.driver.find_element(By.CLASS_NAME, '_1ns0i7c').click()
+            phone_elements = self.driver.find_elements(By.XPATH, '//div[@class="_b0ke8"]/a[contains(@href, "tel:")]')
+            phones = [elem.get_attribute('href').replace('tel:', '') for elem in phone_elements]
+            phones = '\n'.join(phones)
+            sleep(1)
+            return phones
+        except:
+            return phones
+
+    def table_exel(self, data):
+        # Создание новой книги и листа
+        wb = Workbook()
+        ws = wb.active
+
+        # Задание шаблона для заголовка
+        header_font = Font(bold=True, color="FFFFFF")
+        header_fill = PatternFill("solid", fgColor="4F81BD")
+
+        headers = ["ID", "Название", "Адрес", "Сайт", "ИП/ИНН", "Остановка", "Ссылка", "Рейтинг", "Отзывы", "Телефон",
+                   "Email"]
+
+        for col_num, header in enumerate(headers, 1):
+            col_letter = chr(64 + col_num)
+            cell = ws[f"{col_letter}1"]
+            cell.value = header
+            cell.font = header_font
+            cell.fill = header_fill
+
+        for row_num, row_data in enumerate(data, 2):
+            for col_num, cell_data in enumerate(row_data, 1):
+                ws.cell(row=row_num, column=col_num, value=cell_data)
+
+        # Сохраняем файл
+        wb.save("sample.xlsx")
+        # Добавляем данные (это можно сделать согласно вашему шаблону)
 
 
 class CardSearcher:
@@ -55,22 +160,25 @@ class CardSearcher:
         self.slider = None
         self.link = link
 
-    def accept_cookie(self):
-        acc = self.driver.find_element(By.CLASS_NAME, '_euwdl0').click
+    # def make_dirs(self):
+    #     directory = f'links/test/{type_org}'
+    #     company_collection = 'links/href_collection'
+    #     # Проверяем, существует ли файл и директории
+    #     if not os.path.exists(company_collection):
+    #         os.makedirs(company_collection)
 
-    def make_dirs(self):
-        directory = f'links/test/{type_org}'
-        company_collection = 'links/href_collection'
-        # Проверяем, существует ли файл и директории
-        if not os.path.exists(company_collection):
-            os.makedirs(company_collection)
-
-    def page_parsing(self, city, district, type_org_ru, type_org):
+    def check_new_firm(self, city, district, type_org_ru, type_org):
         self.driver.get(self.link)
         self.driver.maximize_window()
-        self.accept_cookie()
-        self.make_dirs()
+        try:
+            with open('firms_dict.json') as file:
+                firm_list = json.load(file)
+        except Exception:
+            with open('firms_dict.json', 'w') as file:
+                json.dump({}, file)
+                firm_list = {}
 
+        fresh_firms = {}
         hrefs_district = []
         # Запрос
         request = city + ' ' + district + ' ' + type_org_ru
@@ -87,73 +195,101 @@ class CardSearcher:
         find_places = self.driver.find_element(By.XPATH, '//div[@class="_nude0k3"]/a[@class="_rdxuhv3"]').text
         number_places = int(''.join(filter(str.isdigit, find_places)))
         print(f'Места: {number_places}')
-        i = 1
-        lim = (number_places / 12).__ceil__()
+
         # поиск и сбор ссылок на компании
-        while '/filters/sort%3Dopened_time' not in driver.current_url:
+        while '/filters/sort%3Dopened_time' not in self.driver.current_url:
             sleep(0.001)
-        while not i > lim:
-            sleep(0.5)
-            current_url = driver.current_url
-            next_page = current_url.replace("/filters/sort%3Dopened_time", f"/filters/sort%3Dopened_time/page/{i}")
-            driver.get(next_page)
-            # print(next_page)
 
-            find_cards = self.driver.find_element(By.CLASS_NAME, '_z72pvu').find_element(By.CLASS_NAME, '_awwm2v')
-            link_elements = find_cards.find_elements(By.CSS_SELECTOR, 'a')
-            hrefs = [href.get_attribute('href') for href in link_elements]
+        find_cards = self.driver.find_element(By.CLASS_NAME, '_z72pvu').find_element(By.CLASS_NAME, '_awwm2v')
+        link_elements = find_cards.find_elements(By.CSS_SELECTOR, 'a')
+        hrefs = [href.get_attribute('href') for href in link_elements]
 
-            # чистка ссылок от рекламы
-            hrefs = filter_links(hrefs)
-            hrefs_district += hrefs
-
-            hrefs_district = list(set(hrefs_district))
-            print(len(list(set(hrefs_district))))
-            i = i + 1
-
+        # чистка ссылок от рекламы
+        hrefs = filter_links(hrefs)
+        hrefs_district += hrefs
+        hrefs_district = list(set(hrefs_district))
         for href in hrefs_district:
-            match = re.search(r'(https://2gis\.ru/moscow/firm/\d+)', href)
-            if match:
-                unique_hrefs.append(match.group(1))
-        try:
-            with open(f'links/href_collection/companies.json', 'r', encoding='utf-8') as f:
-                hrefs = json.load(f)['1']
-            old_hrefs = list(hrefs)
-        except Exception:
-            old_hrefs = []
+            clean_url = href.split('?stat')[0]
+            firm_id = href.split('/firm/')[1].split('?')[0]
+            if firm_id in firm_list:
+                continue
+            else:
+                firm_list[firm_id] = {
+                    'url': clean_url
+                }
 
-        # Находим записи, которые отсутствуют в старом списке
-        hrefs_add = [record for record in unique_hrefs if record not in hrefs]
-        old_hrefs.append(hrefs_add)
-        print(f'Unique: {len(hrefs_add)}')
+                fresh_firms[firm_id] = {
+                    'url': clean_url
+                }
+        with open("firms_dict.json", 'w') as file:
+            json.dump(firm_list, file, indent=1, ensure_ascii=False)
 
-        directory = f'links/test/{type_org}'
-        company_collection = 'links/href_collection'
-
-        # Проверяем, существует ли файл и директории
-        if not os.path.exists(company_collection):
-            os.makedirs(company_collection)
-        with open(f'links/href_collection/companies.json', 'w') as file:
-            json.dump({'1': old_hrefs}, file)
-        with open(f'links/test/{type_org}/{request}.json', 'w') as file:
-            json.dump({'1': unique_hrefs}, file)
+        return fresh_firms
 
 
-if __name__ == '__main__':
-    start_time = time()
-    unique_hrefs = []
+# def first_run():
+#     for type_org in ['sport']:
+#         i = 0
+#         # for district in districts:
+#         for district in ['Район сокол']:
+#             i += 1
+#             print(f"[INFO] Район: {i}/{len(districts)}")
+#             driver = webdriver.Chrome()
+#             grabber = CardSearcher(driver)
+#             grabber.page_parsing(city="Москва", district=district, type_org_ru=type_org_mapping[type_org],
+#                                  type_org=type_org)
 
-    for type_org in ['sport']:
+
+def update_list():
+    fresh_firms_list = {}
+    for type_org in ['sport', 'flowers']:
         i = 0
+
         # for district in districts:
         for district in ['Район сокол']:
             i += 1
             print(f"[INFO] Район: {i}/{len(districts)}")
             driver = webdriver.Chrome()
             grabber = CardSearcher(driver)
-            grabber.page_parsing(city="Москва", district=district, type_org_ru=type_org_mapping[type_org],
-                                 type_org=type_org)
+            fresh_firms = grabber.check_new_firm(city="Москва", district=district,
+                                                 type_org_ru=type_org_mapping[type_org],
+                                                 type_org=type_org)
+            fresh_firms_list.update(fresh_firms)
+            if fresh_firms_list != 0:
+                print(f'[Новыe записи] {district}: {len(fresh_firms_list)}')
+            # if new_firms != 0:
+            #     print(f'Новых записей: {len(new_firms)}')
+            #     parse_info()
+            #     ss = [i['url'] for i in new_firms.values()]
+            #     for s in ss:
+            #         print(s)
+
+    with open(f"fresh_firms_dict.json", 'w') as file:
+        json.dump(fresh_firms_list, file, indent=1, ensure_ascii=False)
+    with open(f"backups/fresh_firms_dict_{cur_time}.json", 'w') as file:
+        json.dump(fresh_firms_list, file, indent=1, ensure_ascii=False)
+
+
+def parse_info():
+    try:
+        with open('fresh_firms_dict.json') as file:
+            firm_list = json.load(file)
+    except Exception:
+        print('Не получилось открыть файл')
+    urls = []
+    driver = webdriver.Chrome()
+    parser = Parser(driver)
+    for k, v in firm_list.items():
+        urls.append(f"{v['url']}")
+    parser.host(urls)
+
+
+if __name__ == '__main__':
+    start_time = time()
+    cur_time = datetime.datetime.now().strftime('%d_%m_%Y_%H_%M')
+    # first_run()
+    # update_list()
+    parse_info()
 
     end_time = time()
     print(f'Прошло времени: {(end_time - start_time):.1f}')
-    # asyncio.run(main())
---
+++ soup_content.py
@@ -0,0 +1,247 @@
+import re
+from time import sleep
+from urllib.parse import urljoin, urlparse
+
+from constants import MY_HEADERS, LINK_NAMES, STATIONS, SITE_PATTERNS
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin, urlparse
+
+
+def process_link(base_url, link):
+    parsed_link = urlparse(link)
+
+    if parsed_link.scheme or parsed_link.netloc:  # Полная ссылка
+        return link
+    elif parsed_link.path.startswith('/'):  # Ссылка со слэшом в начале
+        return urljoin(base_url, link)
+    else:  # Дополняющая ссылка без слэша
+        return urljoin(base_url, '/' + link)
+
+
+class SoupContent(object):
+    def get_name(self, soup_content):
+        name = ''
+        try:
+            for data in soup_content.find_all("h1", {"class": "_tvxwjf"}):
+                name = data.text
+            return name
+        except Exception:
+            return ""
+
+    def get_phone(self, soup_content):
+        try:
+            phones = []
+            for data1 in soup_content.find_all('div', {"class": "_b0ke8"}):
+                print(data1)
+                for data in soup_content.find_all('a', {"class": "_2lcm958"}, href=True):
+                    if data['href'].startswith('tel:'):
+                        phones.append(data['href'].replace("tel:", ""))
+            return '\n'.join(phones)
+        except Exception:
+            print('Не нашло телефоны')
+            return ""
+
+    def get_email(self, soup_content):
+        email = ''
+        try:
+            for data in soup_content.find_all("div", {"class": "_49kxlr"}):
+                temp = data.text
+                if '@' in temp:
+                    email = temp
+                    break
+            return email
+        except Exception:
+            return ""
+
+    def get_address(self, soup_content):
+        try:
+            text = [outer.find('a', class_='_2lcm958').text for outer in soup_content.find_all('span', class_='_14quei') if outer.find('a', class_='_2lcm958')]
+            text = ' '.join(text)
+            text = text.replace("\u200B", "").replace(u"\xa0", u" ")
+            address = text
+            return address
+        except Exception:
+            return ""
+
+    def get_website(self, soup_content):
+        website = ''
+        try:
+            for a in soup_content.find_all('a', {'class': '_1rehek'}):
+                link_text = a.text
+                # Проверяем, не содержит ли текст ссылки http://, https:// или www.
+                for s in SITE_PATTERNS:
+                    if s in link_text:
+                        website = link_text
+                        break
+            return website
+        except Exception:
+            return ""
+
+    def get_rating(self, soup_content):
+        rating = ""
+        try:
+            for data in soup_content.find_all("div", {"class": "_y10azs"}):
+                rating += data.getText()
+            return rating
+        except Exception:
+            return ""
+
+    def get_reviews(self, soup_content):
+        reviews = ""
+        try:
+            for data in soup_content.find_all("div", {"class": "_jspzdm"}):
+                reviews += data.getText()
+                # reviews = re.sub(r'Написать отзыв', '', reviews)
+                # if reviews == "":
+                #     reviews = soup_content.find("div", {"class": "business-header-rating-view__text"}).text
+            return reviews
+        except Exception:
+            return ""
+
+    def get_category(self, soup_content):
+        category = ""
+        try:
+            data = soup_content.find('div', {'class': "_1idnaau"}).text
+            category = data.replace("\u200B", "")
+            return category
+        except Exception:
+            return ""
+
+    def get_station(self, soup_content):
+        station = ""
+        temp = ""
+        try:
+            for data in soup_content.find_all("span", {"class": "_1w9o2igt"}):
+                temp = data.text.replace("\u200B", "")
+
+                for t in STATIONS:
+                    # print(f'Станция {t} элемент {temp}')
+                    if temp == t:
+                        station = temp
+                        break
+            return station
+        except Exception:
+            print('Не нашло станцию')
+            return ""
+
+    def get_branch(self, soup_content):
+        branch = ""
+        pattern = re.compile(r"\d+ филиал(а|ов)?")
+        try:
+            for span in soup_content.find_all("span", {'class': '_er2xx9'}):
+                for a in span.find_all('a', {'class': '_1rehek'}):
+                    if pattern.search(a.text):
+                        branch = a.text
+                        break
+            return branch
+        except Exception:
+            return ""
+
+    def get_ip_inn(self, web_link):
+        timeout = 5
+        urls = {}
+        # checked_url = []
+        self.checked_url = []
+        found_urls = [web_link]
+        try:
+            response = requests.get(web_link, headers=MY_HEADERS, timeout=timeout)
+            # ee = response.status_code
+            # print(ee)
+            if response.status_code == 200:
+                # Получаем содержимое страницы
+                page_content = response.text
+
+                # Создаем объект BeautifulSoup
+                soup = BeautifulSoup(page_content, 'lxml')
+
+                # Ищем ссылки по названиям и сохраняем URL
+                for name in LINK_NAMES:
+                    try:
+                        link = soup.find('a', text=re.compile(name, re.IGNORECASE))
+                        link = link['href']
+                        if link:
+                            url = process_link(web_link, link)
+                            urls[name] = url
+                            found_urls.append(url)
+                        else:
+                            urls[name] = ''
+                    except Exception:
+                        urls[name] = ''
+        except Exception:
+            print('Error connect')
+
+        # Выводим URL
+        # for name, url in urls.items():
+        #     if url != '':
+        #         print(name + " URL:", url)
+
+        # Выводим найденные URL в виде списка
+        print(f"({len(found_urls)}) Found URLs:", found_urls)
+        try:
+            ip = ''
+            inn = ''
+            str_half = ''
+            str_full = ''
+            for web_link in found_urls:
+                # sleep(1)
+                response = requests.get(web_link, headers=MY_HEADERS, timeout=timeout)
+                if response.status_code == 200:
+                    # print('OK!')
+                    page_content = response.text
+                    soup = BeautifulSoup(page_content, 'lxml')
+
+                    ip_pattern = re.compile(
+                        r'(ИП|Индивидуальный предприниматель)[ :]?(?:(?:[А-ЯЁ][а-яё]+ [А-ЯЁ][а-яё]+ [А-ЯЁ][а-яё]+)|(?:[А-ЯЁ][а-яё]+ [А-ЯЁ]\.(?:\s[А-ЯЁ]\.|[А-ЯЁ]\.))|(?:[А-ЯЁ][а-яё]+))', )  # re.IGNORECASE
+
+                    inn_pattern = re.compile(r'ИНН[:/]?[ ]?(\d{10,12})')
+
+                    found_ip_elements = soup(text=ip_pattern)
+                    found_inn_elements = soup(text=inn_pattern)
+
+                    inn_element_td = soup.find('td', text=re.compile(r'ИНН\s*[:-]?\s*'))
+                    inn_element_span = soup.find('span', text=re.compile(r'ИНН\s*[:-]?\s*'))
+
+                    if found_ip_elements and ip == '':
+                        for element in found_ip_elements:
+                            match = re.search(ip_pattern, element)
+                            if match:
+                                ip_text = match.group()
+                                ip = ip_text
+                                # print(ip)
+                            break
+
+                    if found_inn_elements and inn == '':
+                        for element in found_inn_elements:
+                            match = re.search(inn_pattern, element)
+                            if match:
+                                inn_text = match.group(1)
+                                inn = str("ИНН ") + str(inn_text)
+                                # print(inn)
+                            break
+
+                    if inn_element_td and inn != '':
+                        inn_number = inn_element_td.find_next_sibling('td').text
+                        inn = str(inn_number).strip()
+                    if inn_element_span and inn != '':
+                        inn_number = inn_element_span.find_next_sibling('span').text
+                        inn = str(inn_number).strip()
+
+                    if ip != '' and inn == '':
+                        str_half = f'{ip}'
+                    if ip == '' and inn != '':
+                        str_half = f'{inn}'
+                    if ip and inn != '':
+                        str_full = f'{ip} \n{inn}'
+                        break
+
+            if not str_full and str_half:
+                return str_half
+
+            if str_full:
+                return str_full
+
+        except requests.Timeout:
+            print('Превышено время ожидания (тайм-аут)')
+        except Exception:
+            print("Не удалось получить доступ к странице")
